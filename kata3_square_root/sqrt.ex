959c5a9a334dfa73b25e8f912043478bce258ff5
angiust <angelo.giustiniani@gmail.com>
2023-11-07 17:55:50 +0100

    added possibility to choose the precision of sqrt

--- kata3_square_root/sqrt.ex
+++ kata3_square_root/sqrt.ex
@@ -1,15 +1,18 @@
 defmodule Numbers do
   def sqrt(n) do
-    c=ceiling_sqrt(n,1)
+    c=ceil_sqrt(n,1)
     cond do
       (c*c - n) > (n-(c-1)*(c-1)) -> c-1
       true -> c
     end
   end
-  defp ceiling_sqrt(n,try) do
+  defp ceil_sqrt(n,try) do
     cond do
       try*try > n -> try
-      true -> ceiling_sqrt(n, try+1)
+      true -> ceil_sqrt(n, try+1)
     end
+  end
+  def sqrt(n,precision) do
+    sqrt(n*:math.pow(10, 2*precision))/:math.pow(10, precision)
   end
 end
