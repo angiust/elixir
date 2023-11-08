2a259fe3fe24de2f14830e56957e385c8ea993ae
angiust <angelo.giustiniani@gmail.com>
2023-11-08 18:13:29 +0100

    added tollerance on sqrt, modified test in order to write only the result of the computation

--- kata3_square_root/sqrt.ex
+++ kata3_square_root/sqrt.ex
@@ -1,12 +1,12 @@
 defmodule Numbers do
   def sqrt(n) do
-    sqrt(n, n)
+    sqrt(n, n, 0.1)
   end
 
-  def sqrt(n, try) do
+  def sqrt(n, try, tollerance) do
     cond do
-      (try*try-n) <= 0.1 -> try
-      (try*try-n) > 0.1 -> sqrt(n,(try*try+n)/(2*try))
+      (try*try-n) <= tollerance -> try
+      (try*try-n) > tollerance -> sqrt(n,(try*try+n)/(2*try), tollerance)
     end
   end
 
