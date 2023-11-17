0fe97bbf8067ba2240787830270af046f79d768b
angiust <angelo.giustiniani@gmail.com>
2023-11-17 19:45:41 +0100

    finally committed 1st kata

--- kata1_binary_search/binary.ex
+++ kata1_binary_search/binary.ex
@@ -1,5 +1,18 @@
 defmodule Binary do
-  def search(tuple, elem) do
-
-  end
+    def search(tuple, value) do    
+        search_interval(tuple, value, 0, tuple_size(tuple))
+    end
+    def search_interval(_tuple, _value, first, last) when first>=last do
+        false    
+    end
+    def search_interval(tuple, value, first, last) when first<last do
+        middle = div(first+last, 2)
+        try = elem(tuple, middle)
+        cond do
+            try == value -> true
+            try < value -> search_interval(tuple, value, middle+1, last)
+            try > value -> search_interval(tuple, value, first, middle)
+        end
+    end
 end
+
